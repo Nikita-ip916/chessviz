033a74408c4ced615b0db07a6ae5414876410488
Nikita Chigvintsev <nikmail007@gmail.com>
2020-02-24 18:24:54 +0700

    Editing output of figures

--- checkchess.c
+++ checkchess.c
@@ -3,7 +3,7 @@
 const int n = 9;
 int main()
 {
-    char a[n][n];
+    int a[n][n];
     int i, j;
     for (i = 0; i < n; i++) {
         for (j = 0; j < n; j++) {
@@ -13,8 +13,8 @@ int main()
     for (i = 1; i < n; i++) {
         a[i - 1][0] = 48 + n - i;
         a[n - 1][i] = i + 96;
-        a[1][i - 1] = 112;
-        a[6][i - 1] = 80;
+        a[1][i] = 112;
+        a[6][i] = 80;
     }
     a[0][1] = 114;
     a[0][2] = 110;
