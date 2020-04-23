3a01857936eec3561bed76943c1f26c03f9dd4f0
Nikita Chigvintsev <nikmail007@gmail.com>
2020-04-23 17:32:47 +0700

    Added same test to check multitesting

--- test/board_test.c
+++ test/board_test.c
@@ -1,6 +1,5 @@
-#include <sum.h>
-
 #include <ctest.h>
+#include <sum.h>
 
 CTEST(arithmetic_suite, simple_sum)
 {
@@ -13,5 +12,18 @@ CTEST(arithmetic_suite, simple_sum)
 
     // Then
     const int expected = 3;
+    ASSERT_EQUAL(expected, result);
+}
+CTEST(arithmetic_suite2, simple_sum)
+{
+    // Given
+    const int a = 34;
+    const int b = 52;
+
+    // When
+    const int result = sum(a, b);
+
+    // Then
+    const int expected = 86;
     ASSERT_EQUAL(expected, result);
 }
